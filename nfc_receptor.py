a57ab0d327d2345e0a80bd6d008f61a27bd0ec8d
Miguel Ángel <milojama@gmail.com>
2023-10-07 14:00:48 +0200

    Comienzo clase nfc receptor. Avanza #51

---
+++ nfc_receptor.py
@@ -0,0 +1,14 @@
+class Nfc:
+
+    def __init__(self):
+        pass
+
+    def recibirNfc(self):
+        pass
+
+    def enviarNfc(self):
+        pass
+
+    def inicializarComunicacion(self):
+        self.recibirNfc()
+        self.enviarNfc()
