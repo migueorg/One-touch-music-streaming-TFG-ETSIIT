eae7eddb81172c56cb233267ba752cc387366e70
Miguel Ángel <milojama@gmail.com>
2023-10-31 18:16:26 +0100

    Solicito permisos captura de audio Android. Avanzo #52

--- src/Android/app/src/main/java/com/migueorg/otms/MainActivity.kt
+++ src/Android/app/src/main/java/com/migueorg/otms/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.migueorg.otms
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestOnePermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            if (it) {
+                Log.d(TAG, "onActivityResult: PERMISSION GRANTED")
+            } else {
+                Log.e(TAG, "onActivityResult: PERMISSION DENIED")
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(!hasAudioPermission()){
+            requestOnePermission.launch(Manifest.permission.RECORD_AUDIO)
+        }else{
+            Toast.makeText(this@MainActivity, "Ya están los permisos dados.", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
+    private fun hasAudioPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(this,
+            Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+    }
+
 }
