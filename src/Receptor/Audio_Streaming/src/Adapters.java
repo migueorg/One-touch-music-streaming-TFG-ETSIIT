1bbe79e425761c5b8b6b007c2f537da32603f67f
Miguel Ángel <milojama@gmail.com>
2023-11-07 14:12:13 +0100

    Implemento reproducción de audio desde array. Avanzo #54 #51

--- src/Receptor/Audio_Streaming/src/Adapters.java
+++ src/Receptor/Audio_Streaming/src/Adapters.java
@@ -1,4 +1,12 @@
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.SourceDataLine;
+
 public class Adapters implements Ports {
+
+    static AudioFormat formato;
+
     @Override
     public byte[] iniciaServidorAudio() {
         new UnsupportedOperationException("Not implemented yet");
@@ -7,6 +15,22 @@ public class Adapters implements Ports {
 
     @Override
     public void reproduceAudio(byte[] audio) {
-        new UnsupportedOperationException("Not implemented yet");
+        try {
+
+            DataLine.Info dataLineInfo = new DataLine.Info(SourceDataLine.class, formato);
+            SourceDataLine sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
+
+            sourceDataLine.open(formato);
+            sourceDataLine.start();
+
+            sourceDataLine.write(audio, 0, audio.length);
+
+            sourceDataLine.drain();
+            sourceDataLine.close();
+
+        } catch (Exception e) {
+            System.out.println("Error al intentar reproducir");
+            e.printStackTrace();
+        }
     }
 }
