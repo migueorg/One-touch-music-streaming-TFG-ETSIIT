b69a516005399cf95356a692bba6246033b2b5f5
Miguel Ángel <milojama@gmail.com>
2023-11-11 22:03:10 +0100

    Creo fichero ndef con mensaje custom. Avanzo #70 y #51

--- src/Receptor/NFC_Usage/src/adapters.py
+++ src/Receptor/NFC_Usage/src/adapters.py
@@ -1,4 +1,5 @@
 from ports import NfcUsagePort
+from binascii import unhexlify
 
 class NfcUsageAdapter(NfcUsagePort):
 
@@ -6,4 +7,18 @@ class NfcUsageAdapter(NfcUsagePort):
         pass
 
     def createNdefFile(self, ip):
-        pass
+        file = open("ip_actual.ndef","wb")
+
+        if(len(ip)==11): #Cabecera para IP 192.168.1.x
+            tam = '0E'
+        elif(len(ip)==12): #Cabecera para IP 192.168.1.xx
+            tam = '0F'
+        else: #Cabecera para IP 192.168.1.xxx
+            tam = '10'
+        
+        hex_1 = 'D101' + tam + '5402656E' #Cabecera
+        hex_2 = ip.encode("utf-8").hex() #IP
+        bin_s = unhexlify(hex_1+hex_2)
+
+        file.write(bin_s)
+        file.close()
